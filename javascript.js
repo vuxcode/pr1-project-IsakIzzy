6ab7071922cccf4835f4095dc830079e05d7d76c
IsakIzzy <74078015+IsakIzzy@users.noreply.github.com>
2023-04-27 15:43:06 +0200

    Now the user can add ingredients

--- javascript.js
+++ javascript.js
@@ -2,8 +2,8 @@
 var runnerButton = document.getElementById("popupRunner");
 var closeButton = document.getElementById("popupCloser");
 var itemArray = ["","Chicken", "Yellow Onions", "Bell Pepper", "Fajita Seasoning", "Oil", "Sour Cream", "Salsa", "Lime", "Whole Wheat Tortillas"];
-const selector = document.getElementById("add-ingredients-select");
-var amountSelector = document.getElementById("amount-selector");
+var selector = document.getElementById("add-ingredients-select");
+var amountSelector = document.getElementById("amountSelector");
 
 function popupRunner()
 {
@@ -14,6 +14,7 @@ function popupRunner()
 function popupCloser()
 {
  document.getElementById('popup').style.visibility="hidden";
+ document.getElementById('amountSelector').style.visibility="hidden";  
  console.log("CloseButton clicked");
 }
 //JS for changing text based on selection. 
@@ -29,21 +30,56 @@ function updateText()
     // Depending on which selection the user makes, I want the text to change into the most reasonable value for counting the product.  
     if (selector.value == "")
     {
-        document.getElementById('amount-selector').style.visibility="hidden";    
-        console.log("Working?");
-        document.getElementById("empty").innerHTML = "";
+        document.getElementById('amountSelector').style.visibility="hidden";    
+        document.getElementById("ingredient-type-text").innerHTML= "";
     }
     else if (selector.value == "Chicken")
-    {
-        document.getElementById('amount-selector').style.visibility="visible";    
-        document.getElementById("grams").innerHTML = "grams";
+    {        
+        document.getElementById('amountSelector').style.visibility="visible";    
+        document.getElementById("ingredient-type-text").innerHTML= "grams";
     }   
-    else if (selector.value == "Yellow Onions", "Bell Pepper", "Lime")
+    else if (selector.value == "Yellow Onions"|| selector.value ==  "Bell Pepper"|| selector.value == "Lime"|| selector.value == "Whole Wheat Tortillas")
     {
-        document.getElementById("pieces").innerHTML = "pieces";
+        document.getElementById('amountSelector').style.visibility="visible"; 
+        document.getElementById("ingredient-type-text").innerHTML= "pieces";
     }
+    else if (selector.value == "Sour Cream")
+    {
+        document.getElementById('amountSelector').style.visibility="visible"; 
+        document.getElementById("ingredient-type-text").innerHTML= "decilitres";
+    }
+    else if (selector.value == "Fajita Seasoning" || selector.value == "Salsa")
+        {
+            document.getElementById('amountSelector').style.visibility="visible"; 
+            document.getElementById("ingredient-type-text").innerHTML= "packages";
+        }
+        else if (selector.value == "Oil")
+        {
+        document.getElementById('amountSelector').style.visibility="visible"; 
+        document.getElementById("ingredient-type-text").innerHTML= ".";
+    }
+
 
     //function revealAmountSelector
 
 }
+
+ function addIngredientsButton()
+{
+    // Create a list node.
+var node = document.createElement("li");
+
+// Create a text node
+
+var textnode = document.createTextNode(selector.value + " " + amountSelector.value);
+
+node.appendChild(textnode);
+document.getElementById("storageIngredientsList").appendChild(node);
+    console.log ("You are trying to add " + selector.value + amountSelector.value + " to the inventory");
+    // Close down the popup
+    popupCloser();
+
+
+}
+
 
